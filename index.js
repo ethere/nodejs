cce21bfe2848020d2ebf30fae9b3d413405e8c3d
ethere <3318216036@qq.com>
2020-08-18 17:56:46 +0800

    write file

--- index.js
+++ index.js
@@ -1,8 +1,35 @@
 const fs = require('fs');
 const path = require('path');
 
-const filename = path.resolve(__dirname,'./1.txt');
+const from = path.resolve(__dirname, './1.txt');
+const to = path.resolve(__dirname, './2.txt');
 
-fs.unlink(filename,()=>{
-    console.log('文件删除成功')
-})
+// const rs = fs.createReadStream(from);
+// const ws = fs.createWriteStream(to);
+// console.time('方式一')
+// rs.on('data',chunk=>{
+//     let flag = ws.write(chunk);
+//     if(!flag){
+//         rs.pause()
+//     }
+// })
+
+// ws.on('drain',()=>{
+//     rs.resume()
+// })
+
+// console.time('方式二')
+// rs.pipe(ws)
+
+// rs.on('end',()=>{
+//     console.log('复制完成');
+//     // console.timeEnd('方式一');
+//     console.timeEnd('方式二')
+// })
+
+console.time('方式三');
+fs.promises.readFile(from).then(data => {
+    fs.promises.writeFile(to, data).then(() => {
+        console.timeEnd('方式三')
+    })
+});
