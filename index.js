4e02f0b48a3b04ea7762d4a8e019cba5cf9d3a71
ethere <3318216036@qq.com>
2020-10-14 21:10:06 +0800

    services

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-require('./modules/sync')
+const { serialize } = require('v8');
+const Service = require('./services/adminServices.js');
+Service.addAdmin({
+    account:"2018",
+    name:'test',
+    password:'test'
+})
+Service.addAdmin({
+    account:"2017",
+    name:"huifang",
+    password:"123456"
+})
+Service.deleteAdmin(2).then(res=>{
+    console.log(res)
+})
+Service.updateAdmin(1,{
+    account:"2019",
+    name:'test',
+    password:'test'
+})
--- services/adminServices.js
+++ services/adminServices.js
@@ -2,31 +2,40 @@ const Admin = require('../modules/Admin');
 
 exports.addAdmin = async function(adminObj){
    //方式一
-   const ins = Admin.build(adminObj);
-   await ins.save();
+//    const ins = Admin.build(adminObj);
+//    await ins.save();
 
    //方式二
-   //const ins = await Admin.create(adminObj);
+   const ins = await Admin.create(adminObj);
 
    console.log('已添加到数据库啦')
    return ins.toJSON()
 }
 exports.deleteAdmin = async function(adminId){
     //方式一
-    const ins = await Admin.findByPk(adminId);
-    if(ins){
-        ins.destroy()
-    }
+    // const ins = await Admin.findByPk(adminId);
+    // if(ins){
+    //     ins.destroy()
+    // }
 
     //方式二
-    // Admin.destroy({
-    //     where:adminId
-    // })
-    console.log('删除成功啦')
+    Admin.destroy({
+        where:adminId
+    })
+    console.log('删除成功啦');
+    return ins;
 }
 exports.updateAdmin = async function(adminId,adminObj){
     //方式一
     const ins = Admin.findByPk(adminId);
+    ins.name = adminObj.name;
+    ins.password = adminObj.password;
     
     //方式二
+    Admin.update({
+        where:adminId,
+        adminObj
+    })
+    console.log('更新成功啦')
+    return res;
 }
