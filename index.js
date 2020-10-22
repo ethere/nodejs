a15508cbf7acf75dd68c95545c692d5364640fbd
ethere <3318216036@qq.com>
2020-10-22 19:30:03 +0800

    moment

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
-const AdminSev = require('./services/adminServices');
+const moment = require("moment");
+moment.locale("zh-cn")
+// console.log(moment().toString());
+// console.log(moment.utc().toString());
 
-AdminSev.addAdmin({
-    name:'ba',
-    password:'adadfk',
-    account:'2092'
-}).then(res=>{
-    console.log(res)
-})
+// console.log(moment().valueOf(),+moment());
+// console.log(moment.utc().valueOf(),+moment.utc());
+
+// console.log(moment(0).toString(),+moment(0));
+// console.log(moment.utc(0).toString(),+moment.utc(0));
+
+// const value = '1999-10-19 00:00:00';
+// console.log(moment(value).toString(),+moment(value));
+// console.log(moment.utc(value).toString(),+moment.utc(value));
+
+const formats = ["YYYY-MM-DD HH:mm:ss","YYYY-M-D H:m:s","x"];
+// console.log(moment.utc("1979-12-01 00:00:00",formats,true).toString());
+// console.log(moment.utc("1970-1-1 0:0:0",formats,true).toString());
+// console.log(moment.utc(0,formats,true).toString());
+// console.log(moment.utc("Thu Jan 01 1970 00:00:00 GMT+0000",formats,true).toString());
+
+// const m = moment.utc("2015-1-5 23:00:01",formats,true);
+// console.log(m.format("YYYY年MM月DD日 HH点mm分ss秒"));
+// console.log(m.local().format("YYYY年MM月DD日 HH点mm分ss秒"));
+// const local = moment("2015-1-5 23:00:00",formats,true);
+// console.log(local.utc().format("YYYY年MM月DD日 HH点mm分ss秒"))
+
+// const m = moment.utc("2020-4-14 9:00:01",formats,true);
+// console.log(m.local().fromNow())
--- note.md
+++ note.md
@@ -1,22 +1,26 @@
-hash算法
+UTC 格林威治时间
 
-单向加密：只能加密不能解密
-
-加密固定长度的字符串
-
-同样的源字符串，加密后得到结果固定
+时间戳表示的是utc时间的差异，与本地时间没有关系
 
 1. 安装
-```powershell
-npm i md5
-```
+npm i moment
+
 2. 导入
+
 ```js
-const md5 = require('md5')
-```
-3. 使用
-```js
-const result = md5('')
+const moment = require("moment")
 ```
 
-最好的加密压缩算法
+3. 使用
+
+在服务器建议用moment.utc()
+
+使用日期令牌转换 moment.utc(time,formats,true).toString()
+
+显示（发生在客户端）moment.utc(time,formats,true).local().format(format)  将utc时间按format转为本地时间
+
+moment(time,formats,true).utc().format(format) 将本地时间按format转为utc时间
+
+m.local().fromNow()  距离当前相差的时间
+
+moment.locale("zh-cn") 语言库
