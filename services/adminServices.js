9ab9cbea274bff4e9e104cf3ede9c3533b6fbcc7
ethere <3318216036@qq.com>
2020-10-22 09:57:02 +0800

    md5

--- services/adminServices.js
+++ services/adminServices.js
@@ -1,59 +1,40 @@
 const Admin = require('../models/Admin');
+const md5 = require('md5')
 
 exports.addAdmin = async function (adminObj) {
-    //方式一
-    //    const ins = Admin.build(adminObj);
-    //    await ins.save();
-
-    //方式二
+    adminObj.password = md5(adminObj.password)
     const ins = await Admin.create(adminObj);
-
     return ins.toJSON()
 }
 exports.deleteAdmin = async function (adminId) {
-    //方式一
-    // const ins = await Admin.findByPk(adminId);
-    // if(ins){
-    //     ins.destroy()
-    // }
-
-    //方式二
     const ins = await Admin.destroy({
         where: {
             id:adminId
         }
     })
-
-    console.log('删除成功啦')
     return ins;
 }
 exports.updateAdmin = async function (id, adminObj) {
-    // 方式1
-    // 1. 得到实例
-    // const ins = await Admin.findByPk(id);
-    // ins.name = adminObj.name;
-    // // 2. 保存
-    // ins.save();
-
-    // 方式2
+    if(adminObj.password){
+        adminObj.password = md5(adminObj.password)
+    }
     const result = await Admin.update(adminObj, {
         where: {
             id,
         },
     });
-
-    console.log("更新成功啦")
-    return result;
+    return result.toJSON();
 }
 
 exports.login = async function (name,password){
+   password = md5(password)
    const result = await Admin.findOne({
         where:{
             name,
             password,
         }
     });
-    if(result && result.name === name && result.password === password){
+    if(result && result.name === name){
         return result.toJSON();
     }
     return null;
